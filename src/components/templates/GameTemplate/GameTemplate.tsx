2fe5b8fb9c2ff332fc0c107c34840061782af980
진현성 <gostroad37@gmail.com>
2026-06-14 16:37:38 +0900

    refactor: 스타일 및 레이아웃 개선을 위한 코드 정리

--- src/components/templates/GameTemplate/GameTemplate.tsx
+++ src/components/templates/GameTemplate/GameTemplate.tsx
@@ -14,17 +14,15 @@ export interface GameTemplateProps {
 }
 const GameTemplate: FC<GameTemplateProps> = ({ title, href, children }) => {
   return (
-    <>
-      <div className="game-template">
-        <NavigationBar>
-          <Link href={href}>
-            <Image src={arrowURL} alt="뒤로가기" />
-          </Link>
-        </NavigationBar>
-        <MapTitle title={title} />
-        {children}
-      </div>
-    </>
+    <div className="game-template">
+      <NavigationBar>
+        <Link className="nav-back" href={href}>
+          <Image src={arrowURL} alt="뒤로가기" />
+        </Link>
+      </NavigationBar>
+      <MapTitle title={title} />
+      <div className="game-template-content">{children}</div>
+    </div>
   );
 };
 
